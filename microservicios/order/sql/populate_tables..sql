b25a03a70b9117a87e761b0ecdbbd3d17419afbe
toopazo <toopazo@proton.me>
2025-04-23 16:05:49 -0400

    se logra enlazar spring boot con Oracla DB usando JPA

---
+++ microservicios/order/sql/populate_tables..sql
@@ -0,0 +1,14 @@
+select user
+  from dual;
+select *
+  from user_tables;
+
+select current_timestamp
+  from dual;
+
+insert into menu ( name ) values ( 'Papas fritas con pollo al jugo' );
+insert into menu ( name ) values ( 'Papas fritas con bistec' );
+insert into menu ( name ) values ( 'Papas mayo con bistec' );
+
+select *
+  from menu;
--- microservicios/order/src/main/java/cl/dsy1103/order/controller/OrderController.java
+++ microservicios/order/src/main/java/cl/dsy1103/order/controller/OrderController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
