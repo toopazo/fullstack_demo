c7efc12c519d2154a542fa708d1e60b11eb1bbeb
toopazo <toopazo@proton.me>
2025-05-02 11:49:56 -0400

    se logra hacer funcionar JPA con postgres y oracle

--- microservicios/order/src/main/java/cl/dsy1103/order/OrderApplication.java
+++ microservicios/order/src/main/java/cl/dsy1103/order/OrderApplication.java
@@ -10,22 +10,4 @@ public class OrderApplication {
 		SpringApplication.run(OrderApplication.class, args);
 	}
 
-	// @GetMapping("/api/v1/hello")
-	// public String hello(@RequestParam(value = "name", defaultValue = "World")
-	// String name) {
-	// return String.format("Hello %s!", name);
-	// }
-
-	// @GetMapping("/api/v1/suma")
-	// public long suma(@RequestParam(name = "a", required = true) long a,
-	// @RequestParam(name = "b", required = true) long b) {
-	// return a + b;
-	// }
-
-	// @GetMapping("/api/v1/multi")
-	// public long multi(@RequestParam(name = "a", required = true) long a,
-	// @RequestParam(name = "b", required = true) long b) {
-	// return a * b;
-	// }
-
 }
